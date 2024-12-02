7d381cc5e4170d29596e246c46ada502326a5e58
ChipinDip <chipindip5674@gmail.com>
2024-12-01 17:29:31 -0700

    Audio

---
+++ ProjectLargio/Assets/Scenes/PlaySoundOnTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlaySoundOnTrigger : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.LogError("No AudioSource component found on this GameObject.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+        }
+    }
+}
